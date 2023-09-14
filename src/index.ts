46153b960f43b6a4768fbe55905a412ae5d0e080
jeremy <jeremyfelix41@gmail.com>
2023-09-14 17:00:30 +0200

    Installation du CORS

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,11 @@ console.log('Hello world');
 
 import express from "express"
 import "dotenv/config"
+import cors from "cors"
 
 const app = express()
+app.use(cors())
 const port = parseInt(process.env.PORT as string)
-
 
 
 import { DataTypes, Sequelize } from "sequelize"
