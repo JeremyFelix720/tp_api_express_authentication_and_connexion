be42d7e07de513bb1ecbd3ce3565147a8aaa4e74
jeremy <jeremyfelix41@gmail.com>
2023-12-06 11:56:10 +0100

    :truck: route to add a free game

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,63 @@
 console.log('Hello world');
 
+import { DataTypes, Sequelize } from "sequelize"
+import bodyParser from "body-parser"
 import express from "express"
 import "dotenv/config"
 import cors from "cors"
 
 const app = express()
-app.use(cors())
 const port = parseInt(process.env.PORT as string)
 
+app.use(cors());
+app.use(bodyParser.json())
 
-import { DataTypes, Sequelize } from "sequelize"
+/*
+interface IMyBodyRequest {
+  name: string,
+  description: string,
+  duration: string,
+}
+*/
 
 const sequelize = new Sequelize({
   dialect: "sqlite",
   storage: "./db.sqlite",
 })
 
+const FreeGameTable = sequelize.define("FreeGameTable", {
+  name: {
+    type: DataTypes.STRING,
+  },
+  description: {
+    type: DataTypes.STRING,
+  },
+  image: {
+    type: DataTypes.STRING,
+  },
+}, {
+  timestamps: false,
+})
+
+const OfficialGameTable = sequelize.define("OfficialGameTable", {
+  name: {
+    type: DataTypes.STRING,
+  },
+  description: {
+    type: DataTypes.STRING,
+  },
+  image: {
+    type: DataTypes.STRING,
+  },
+  price: {
+    type: DataTypes.NUMBER,
+  },
+}, {
+  timestamps: false,
+})
+
+
+sequelize.sync()
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -23,8 +65,45 @@ app.get('/', (req, res) => {
 
 app.get('/toto/', (req, res) => {
     res.send('Toto')
+})
+
+/*
+// Ajouter un utilisateur VOIR CA A LA FIN
+app.post("/api/auth/local/register", async (req, res) => {
+  res.send()
+})
+*/
+
+// Ajouter un jeu gratuit
+app.post("/api/free-games/", async (req, res) => {
+  const freeGameName = req.body.name;
+  const freeGameDescription = req.body.description;
+  const freeGameImage = req.body.image;
+
+  const myNewFreeGame = await FreeGameTable.create({
+    name: freeGameName as string,
+    description: freeGameDescription as string,
+    image: freeGameImage as string,
   })
+
+  console.log(myNewFreeGame);
+  res.json(myNewFreeGame)
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+/*
+// Pour faire le lien entre le code précédent et le fichier "bd.sqlite" ?
+sequelize
+  .sync()
+  // .sync({ force: true })
+  .then(() => {
+    console.log("Base de données synchronisée et prête");
+
+  })
+  .catch(error => {
+    console.error('Erreur de synchronisation:', error);
+  });
+  */
